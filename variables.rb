c91ad56e99d2a472f7f2efda6927c973aaa4fbde
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-02-22 12:56:22 -0700

    Fix string variable names

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
-name = Andy Stilmock
-school = Turing
-location = Lakewood Colorado
+name = "Andy Stilmock"
+school = "Turing"
+location = "Lakewood Colorado"
 age = 33
 family_members = 6
 roommates = 1
@@ -10,10 +10,5 @@ bank_balance = 3.50
 bored = false
 excited = true
 hungry = true
-name = Not Andy Stilmock
-school = Nebraska Wesleyan University
-location = Lincoln Nebraska
-puts [name]
-puts [age]
-puts [remaining_days]
+
 
